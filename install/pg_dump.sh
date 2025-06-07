d77d49d56a315c001042a07b74bd95f52d0274d8
jesusalbujas <jesusramirez35000@gmail.com>
2025-06-07 12:32:48 -0400

    Update pg_dump

--- install/pg_dump.sh
+++ install/pg_dump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker exec -it project-two.postgresql.service pg_dump -U adempiere -Fc -d adempiere > /tmp/adempiere.backup -v
+docker exec -it pst.3.postgresql.service pg_dump -U adempiere -Fc -d adempiere > /tmp/adempiere.backup -v
 echo "Respaldo realizado"
 
 cp /tmp/adempiere.backup /opt/Development/github/postgres-university/postgresql/seed.backup
